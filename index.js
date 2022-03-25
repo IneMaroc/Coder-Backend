72915b5a53eed789a2a626594c1b6fa4645ddb8a
IneMaroc <inemaroc.ok@gmail.com>
2022-03-24 23:51:39 -0300

    Desafio1

--- index.js
+++ index.js
@@ -21,9 +21,23 @@ class User {
       this.books.push({book: name, author: author});
    }
    getBookName () {
-      return this.book.map((bk) => bk.book);
+      return this.books.map((bk) => bk.book);
    }
 }
 
 const user = new User ('Ine', 'Maroc');
 
+user.addPet('Pez');
+user.addBook('El Ojo del Mundo', 'Robert Jordan');
+user.addBook('La Gran Cacería', 'Robert Jordan');
+user.addBook('El Dragon Renacido', 'Robert Jordan');
+console.log(user.getBookName());
+console.log(user.countPets());
+user.addBook('El Aumento de la Sombra', 'Robert Jordan');
+user.addBook('Cielo en Llamas', 'Robert Jordan');
+user.addBook('El Señor del Caos', 'Robert Jordan');
+user.addPet('Perro');
+
+console.log(user.getFullName());
+console.log(user.countPets());
+console.log(user.getBookName());
